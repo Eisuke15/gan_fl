4e59856306d7b67786a1900bad1b96dc6330591a
Eisuke15 <hideyu41691@gmail.com>
2022-10-23 23:54:48 +0900

    use train function

--- fl.py
+++ fl.py
@@ -9,6 +9,7 @@ from torchvision.datasets import MNIST
 from torchvision.utils import save_image
 
 from gan import Discriminator, Generator
+from train import gen_fixed_noise, train
 
 parser = ArgumentParser()
 parser.add_argument('-e', '--nepoch', type=int, help="number of epochs to train for", default=10000)
@@ -44,48 +45,10 @@ fixed_noise = torch.randn(64, args.nz, 1, 1, device=device)
 
 # pre-self training
 # train local model just by using the local data
-for epoch in trange(args.pre_nepoch, desc="pre-self training epoch"):
-    for node_num, (g, d, g_optimizer, d_optimizer, dataloader) in tqdm(enumerate(zip(generators, discriminators, g_optimizers, d_optimizers, train_loaders)), leave=False, total=n_node, desc="node"):
-        g.train()
-        d.train()
-        for images, _ in tqdm(dataloader, leave=False, desc="batch"):
-            ############################
-            # (1) Update D network: maximize log(D(x)) + log(1 - D(G(z)))
-            ###########################
-            # train with real
-            d.zero_grad()
-            real_cpu = images.to(device)
-            batch_size = real_cpu.size(0)
-            label = torch.full((batch_size,), 1, dtype=real_cpu.dtype, device=device)
-
-            output = d(real_cpu)
-            errD_real = criterion(output, label)
-            errD_real.backward()
-            D_x = torch.where(output > 0.5, 1., 0.).mean().item()
-
-            # train with fake
-            noise = torch.randn(batch_size, args.nz, 1, 1, device=device)
-            fake = g(noise)
-            label.fill_(0)
-            output = d(fake.detach())
-            errD_fake = criterion(output, label)
-            errD_fake.backward()
-            D_G_z1 = torch.where(output > 0.5, 1., 0.).mean().item()
-            errD = errD_real + errD_fake
-            d_optimizer.step()
-
-            ############################
-            # (2) Update G network: maximize log(D(G(z)))
-            ###########################
-            g.zero_grad()
-            label.fill_(1)  # fake labels are real for generator cost
-            output = d(fake)
-            errG = criterion(output, label)
-            errG.backward()
-            D_G_z2 = torch.where(output > 0.5, 1., 0.).mean().item()
-            g_optimizer.step()
-    
-        if epoch == args.pre_nepoch - 1:
+for epoch in range(args.pre_nepoch):
+    for node, (g, d, g_optimizer, d_optimizer, dataloader) in enumerate(zip(generators, discriminators, g_optimizers, d_optimizers, train_loaders)):
+        train(dataloader, g, d, g_optimizer, d_optimizer, nz, epoch, args.pre_nepoch, device, conditional, node)
+        if epoch%10 == 0 or epoch == args.pre_nepoch - 1:
             g.eval()
             save_image(g(fixed_noise), f'images/fl/first_z{args.nz}_n{node_num}.png')
 
@@ -113,14 +76,8 @@ for epoch in range(args.nepoch + 1):
 
         g.load_state_dict(global_generator)
         d.load_state_dict(global_discriminator)
-        g.train()
-        d.train()
 
-        errDs = []
-        errGs = []
-        D_x_seq = []
-        D_G_z1_seq = []
-        D_G_z2_seq = []
+        train(dataloader, g, d, g_optimizer, d_optimizer, nz, epoch, args.nepoch, device, conditional, node)
 
         for i, (images, _) in enumerate(dataloader):
             ############################
